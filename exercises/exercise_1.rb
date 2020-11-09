8dcb536e1cb7528675dd991915b15846409e7d95
ericmcgrandle <ericmcgrandle@gmail.com>
2020-11-09 15:14:49 -0800

    move to right files

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -8,15 +8,4 @@ burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: tr
 richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
-@store1 = Store.find_by(id: 1)
-@store1.update(name: "Delta") 
-
-@store2 = Store.find_by(id: 2)
-
-@store3 = Store.find_by(id: 3)
-@store3.destroy
-
-
-
-
-
+puts Store.count
