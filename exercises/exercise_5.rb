4649b7d074d1181bcfa640c211a81a7e92f807fe
ericmcgrandle <ericmcgrandle@gmail.com>
2020-11-09 15:52:32 -0800

    total ann rev, average, count above 1m

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+num_of_stores = Store.count
+total_annual_revenue = Store.sum("annual_revenue")
+
+puts total_annual_revenue
+puts total_annual_revenue / num_of_stores
+
+
+puts Store.where("annual_revenue > 1000000").count
