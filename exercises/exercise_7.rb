58ad98e2c558c6d3131cb05cb244bf6d6540b585
ericmcgrandle <ericmcgrandle@gmail.com>
2020-11-09 16:42:27 -0800

    validation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,13 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Enter a store name"
+store_name = gets.chomp
+
+delta = Store.create(name: "#{store_name}")
+# store = Store.create(name: "#{store_name}", annual_revenue: 300000, mens_apparel: false, womens_apparel: false)
+
+store.errors.messages.each {
+  |e|
+  puts e
+}
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,15 @@
 class Store < ActiveRecord::Base
-  `has_many :employees`
+  has_many :employees
+  validates :name, length: { minimum: 3 } 
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 } 
+  validate :validate_apparel
 
+  def validate_apparel
+    puts mens_apparel
+    puts womens_apparel
+    
+    if !mens_apparel && !womens_apparel
+      errors.add(:validate_apparel, "Must carry something!")
+    end
+  end
 end
